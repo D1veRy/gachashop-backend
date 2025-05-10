274a706556f8d9c064dbf597315868fc8bca97ae
D1veRy <125744591+D1veRy@users.noreply.github.com>
2025-05-10 03:55:08 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -127,7 +127,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', 'your-ip-or-render-ip'], // Добавь свой IP или IP сети Render
+        'allowedIPs' => [
+            '127.0.0.1',
+            '::1',
+            '52.41.36.82,
+54.191.253.12,
+44.226.122.3'
+        ], // Добавь свой IP или IP сети Render
     ];
 
     $config['bootstrap'][] = 'gii';
